1047e3f37ea298695e71840fd6300ef95d800d23
Artjoms <tjemati@gmail.com>
2022-01-06 18:53:23 +0200

    [feature/java1]: Homework progress add

---
+++ src/main/java/Homework/Homework.java
@@ -0,0 +1,20 @@
+package Homework;
+
+public class Homework {
+    public static void main(String[] args) {
+
+//        Short
+        short distanceBetweenRigaAndBerlin = 1200;
+        System.out.println(distanceBetweenRigaAndBerlin);
+
+        Short distanceBetweenRigaAndVilnius = 300;
+        System.out.println(distanceBetweenRigaAndVilnius);
+
+        Short distanceBetweenRigaAndKaunas = 280;
+        System.out.println(distanceBetweenRigaAndKaunas);
+
+
+
+    }
+
+}
