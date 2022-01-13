7b507697fe8f1efb77ddab8de127797e0563007c
Artjoms <tjemati@gmail.com>
2022-01-13 18:35:10 +0200

    [feature/java2]: classwork progress add

---
+++ src/main/java/ArrayPractice.java
@@ -0,0 +1,11 @@
+public class ArrayPractice {
+    public static void main(String [] args) {
+
+        String fruit = "apple";
+        String[] fruits = {"apple", "banana", "orange", "pear", "strawbery", "melon"};
+//         fruits[0] = apple
+//        fruits[1] = banana
+        System.out.println(fruits[1])
+        ;
+    }
+}
